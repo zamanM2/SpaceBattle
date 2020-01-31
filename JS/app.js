b9d6b8811d068ea6c6bb7d19fc285376c561afe6
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-01-31 14:38:20 -0500

    fixed classes

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,40 @@
-class ussSwarch = {
+class ussSwarch {
 constructor(){
 
-  this.hull: 20,
-  this.firepower: 5,
-  this.accuracy: .7,
+  this.hull= 20;
+  this.firepower= 5;
+  this.accuracy= .7;
   }
 }
 
-class alienShips ={
+class alienShips {
   constructor(){
 
   this.maxHull= 6;
   this.minHull =3;
 
-  this.hull: Math.random(),
-  this.firepower: Math.random() ,
-  this.accuracy: Math.random(),
-}
+  this.maxFirepower=4 ;
+  this.minFirepower=2 ;
 
-}
+  this.maxAccuracy=.6 ;
+  this.minAccuracy= .8;
 
-function randomSpec(){
-  for(let i=0; i <= 6; i++){
-//     while (i < ) {
-//   text += "The number is " + i;
-//   i++;
-// }
+  this.hull= Math.random() * (max - min) + min;
+  this.firepower= Math.random() * (max - min) + min;
+  this.accuracy= Math.random() * (max - min) + min;
   }
 
-  Math.random()
 }
+
+// function randomSpec(){
+//   for(let i=0; i <= 6; i++){
+// //     while (i < ) {
+// //   text += "The number is " + i;
+// //   i++;
+// // }
+//   }
+
+//   Math.random()
+// }
+
+console.log("this is working")
