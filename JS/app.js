da62c587c3dd4d9a22684085ead348d7a5df0f10
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-01-31 13:58:14 -0500

    added classes

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+class ussSwarch = {
+constructor(){
+
+  this.hull: 20,
+  this.firepower: 5,
+  this.accuracy: .7,
+  }
+}
+
+class alienShips ={
+  constructor(){
+
+  this.maxHull= 6;
+  this.minHull =3;
+
+  this.hull: Math.random(),
+  this.firepower: Math.random() ,
+  this.accuracy: Math.random(),
+}
+
+}
+
+function randomSpec(){
+  for(let i=0; i <= 6; i++){
+//     while (i < ) {
+//   text += "The number is " + i;
+//   i++;
+// }
+  }
+
+  Math.random()
+}
