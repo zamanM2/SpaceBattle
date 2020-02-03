6b3306b62ea6dcccfe003e17b859673b48c945b0
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-02-03 14:58:44 -0500

    added attack function

--- JS/app.js
+++ JS/app.js
@@ -14,7 +14,20 @@ constructor(){
   const accuracy= .7;
   }
 
+ ussAttack (alienShip){
+  console.log("inside attack func firepower is : " + this.firepower);
+
+
+
+  alienShip.hull = alienShip.hull - this.firepower;
+
+  console.log("inside attack func alienship hull is now  : " + alienShip.hull);
+ // return remain;
 }
+
+
+}
+
 
 class alienShips {
   constructor(){
@@ -45,10 +58,6 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-function attack (){
- let remain= this.hull -(this.firepower*this.accuracy)
- return remain;
-}
 
 let alienShip1 = new alienShips();
 let alienShip2 = new alienShips();
@@ -57,6 +66,9 @@ let alienShip4 = new alienShips();
 let alienShip5 = new alienShips();
 let alienShip6 = new alienShips();
 
+let ussSwarch1 = new ussSwarch();
+
+console.log(ussSwarch1.ussAttack(alienShip1));
 
 
 console.log("this is working")
